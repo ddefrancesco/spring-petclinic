169a6dce4e3f667ea7598db2dadeafc080af17da
Daniele De Francesco <daniele.defrancesco@nttdata.com>
2021-03-21 18:49:57 +0100

    pom.xml: azure deploy plugin configuration/skip checkstyle control main.tf: App Service configuration for Spring Boot

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,4 +44,36 @@ resource "azurerm_mysql_firewall_rule" "pc_mysql_fw" {
   end_ip_address               = "0.0.0.0"
 }
 
+resource "azurerm_app_service_plan" "pc_app_svc_plan" {
+  name                = "tmp-pc-appserviceplan"
+  location            = azurerm_resource_group.pc_res_group.location
+  resource_group_name = azurerm_resource_group.pc_res_group.name
+  kind = "Linux"
+  reserved = "true"
+
+  sku {
+    tier              = "PremiumV2"
+    size              = "P1v2"
+  }
+}
+
+resource "azurerm_app_service" "pc_app_svc" {
+  name = "tmp-pc-app-service"
+  location = azurerm_resource_group.pc_res_group.location
+  resource_group_name = azurerm_resource_group.pc_res_group.name
+  app_service_plan_id = azurerm_app_service_plan.pc_app_svc_plan.id
+
+  https_only          = true
+  site_config {
+    linux_fx_version  = "JAVA|11-java11"
+  }
+
+  app_settings = {
+    "SPRING_PROFILES_ACTIVE" = "mysql"
+    "SPRING_DATASOURCE_URL" = "jdbc:mysql://${azurerm_mysql_server.pc_mysql_rg.fqdn}:3306/${azurerm_mysql_database.pc_mysql_database.name}?useUnicode=true&characterEncoding=utf8&useSSL=true&useLegacyDatetimeCode=false&serverTimezone=UTC"
+    "SPRING_DATASOURCE_USERNAME" = "${azurerm_mysql_server.pc_mysql_rg.administrator_login}@${azurerm_mysql_server.pc_mysql_rg.name}"
+    "SPRING_DATASOURCE_PASSWORD" = azurerm_mysql_server.pc_mysql_rg.administrator_login_password
+  }
+
+}
 
